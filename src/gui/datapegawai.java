24aa04d8d54105879593ec922b3a73268c95544c
Muhammad Helmi <105972423+junaibeca2@users.noreply.github.com>
2023-01-15 13:14:35 +0800

    Initial commit

---
+++ src/gui/datapegawai.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+/**
+ *
+ * @author Asus
+ */
+public class datapegawai {
+    public static void main (String [] args){
+        new tabel_gaji(). setVisible(true);
+    }
+}
